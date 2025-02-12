dcba673c6a91302bf1108b3ee3e7cefedef3c89e
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-02-12 00:30:50 +0000

    Removed unused code

--- javascript.js
+++ javascript.js
@@ -1,12 +1,4 @@
-/* First Attempt - onclick is on the html page */
-// function darkMode() {
-//     var element = document.body;
-//     element.classList.toggle("dark-mode");
-//   }
-
-
-/* Second Attempt - onclick is removed form the html page and added as an event (part of the script) */
-/* Only run script after html is fully loaded */
+/* Only run script to activate dark mode after html is fully loaded */
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.querySelector(".button1");
 
